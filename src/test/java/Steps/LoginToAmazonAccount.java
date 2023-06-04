05484f8273d5424d325c478c48052746fe7809e4
thesamanta <samanta.chowdhury718@gmail.com>
2023-06-04 14:37:29 -0400

    test

--- src/test/java/Steps/LoginToAmazonAccount.java
+++ src/test/java/Steps/LoginToAmazonAccount.java
@@ -1,10 +1,13 @@
 package Steps;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.config.WebDriverManagerException;
 
 public class LoginToAmazonAccount {
 	
@@ -13,6 +16,10 @@ public class LoginToAmazonAccount {
 @Given("I am on the amazon homepage")
 public void i_am_on_the_amazon_homepage() {
     // hey how are u 
+	
+	WebDriverManager.chromedriver().setup();
+	driver = new ChromeDriver();
+	driver.get("https://www.amazon.com/");
 }
 
 @When("I click on the sign in button")
